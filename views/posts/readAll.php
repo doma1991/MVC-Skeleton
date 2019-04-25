6a6442d16e3a97f1149ae4a6cde7b41c60351a7c
Dominika <do_ma1991@hotmail.com>
2019-04-25 21:44:58 +0100

    fixing upload

--- views/posts/readAll.php
+++ views/posts/readAll.php
@@ -21,11 +21,11 @@
 <?php foreach($posts as $post) { ?>
   <p>
     //<?php echo $post->title; ?> &nbsp; &nbsp;
-    <a href='?controller=post&action=read&id=//<?php echo $post->id; ?>'>See post information</a> &nbsp; &nbsp;
+    <a href='?controller=post&action=read&id=<?php echo $post->id; ?>'>See post information</a> &nbsp; &nbsp;
     
     //<?PHP if(isset ($_SESSION['userID'])) {?> 
-    <a href='?controller=post&action=delete&id=//<?php echo $post->id; ?>'>Delete post</a> &nbsp; &nbsp;
-    <a href='?controller=post&action=update&id=//<?php echo $post->id; ?>'>Update post</a> &nbsp;
+    <a href='?controller=post&action=delete&id=<?php echo $post->id; ?>'>Delete post</a> &nbsp; &nbsp;
+    <a href='?controller=post&action=update&id=<?php echo $post->id; ?>'>Update post</a> &nbsp;
 <?php } }?>
   </p>
 
